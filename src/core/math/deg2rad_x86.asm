e630054060f70f74419d0be7ca1fbb9aed1e1c24
Kanaan Kebriti <kanaankebriti@yahoo.com>
2020-05-30 19:18:14 +0430

    add support for x86

---
+++ src/core/math/deg2rad_x86.asm
@@ -0,0 +1,35 @@
+;░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+;░ This file is part of libfastact.										░
+;░																		░
+;░ libfastact is free software: you can redistribute it and/or modify	░
+;░ it under the terms of the GNU General Public License as published by	░
+;░ the Free Software Foundation, either version 3 of the License, or	░
+;░ (at your option) any later version.									░
+;░																		░
+;░ libfastact is distributed in the hope that it will be useful,		░
+;░ but WITHOUT ANY WARRANTY; without even the implied warranty of		░
+;░ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the			░
+;░ GNU General Public License for more details.							░
+;░																		░
+;░ You should have received a copy of the GNU General Public License	░
+;░ along with libfastact.  If not, see <https://www.gnu.org/licenses/>.	░
+;░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+;┌──────────────────────────────┐
+;│ converts degree to radians	│
+;│ input:						│
+;│			deg input			│
+;│ output:						│
+;│			st0 = rad output	│
+;└──────────────────────────────┘
+proc fa_deg2rad stdcall num:QWORD
+	locals
+		pi			dq M_PI
+		divisor		dq 180.000
+	endl
+	movsd	xmm0,[num]
+	divsd	xmm0,[divisor]
+	mulsd	xmm0,[pi]
+	movsd	[num],xmm0
+	fld		[num]
+	ret
+endp
