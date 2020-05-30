4fc9640ee70d38717a3be56bfd1c3790d4eccd28
Kanaan Kebriti <kanaankebriti@yahoo.com>
2020-05-30 18:40:01 +0430

    add support for x86

---
+++ src/core/math/cos_x86.asm
@@ -0,0 +1,28 @@
+;░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+;░ This file is part of libfastact.										░
+;░																		░
+;░ libfastact is free software: you can redistribute it and/or modify	░
+;░ it under the terms of the GNU General Public License as published by	░
+;░ the Free Software Foundation, either version 3 of the License, or	░
+;░ (at your option) any later version.									░
+;░																		░
+;░ libfastact is distributed in the hope that it will be useful,		░
+;░ but WITHOUT ANY WARRANTY; without even the implied warranty of		░
+;░ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the			░
+;░ GNU General Public License for more details.							░
+;░																		░
+;░ You should have received a copy of the GNU General Public License	░
+;░ along with libfastact.  If not, see <https://www.gnu.org/licenses/>.	░
+;░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+;┌──────────────────────────────┐
+;│ compute cosine				│
+;│ input:						│
+;│			number				│
+;│ output:						│
+;│			st0 = cos(number)	│
+;└──────────────────────────────┘
+proc fa_cos stdcall num:QWORD
+	fld		[num]			; load [num] into x87 stack (ST0)
+	fcos					; ST0 = sin(ST0)
+	ret
+endp
