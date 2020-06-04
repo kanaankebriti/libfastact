a7ea933f91ad7837b834ff9e9667d657405fba65
Kanaan Kebriti <kanaankebriti@yahoo.com>
2020-06-04 17:18:49 +0430

    add support for x86

---
+++ src/core/string/strlen_x86.asm
@@ -0,0 +1,34 @@
+;░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+;░ This file is part of libfastact.										░
+;░																		░
+;░ libfastact is free software: you can redistribute it and/or modify	░
+;░ it under the terms of the GNU General Public License as published by	░
+;░ the Free Software Foundation, either version 3 of the License, or	░
+;░ (at your option) any later version.									░
+;░																		░
+;░ libfastact is distributed in the hope that it will be useful,		░
+;░ but WITHOUT ANY WARRANTY; without even the implied warranty of		░
+;░ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the			░
+;░ GNU General Public License for more details.							░
+;░																		░
+;░ You should have received a copy of the GNU General Public License	░
+;░ along with libfastact.  If not, see <https://www.gnu.org/licenses/>.	░
+;░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+;┌──────────────────────────────────┐
+;│ returns length of a string		│
+;│ input:							│
+;│			*source					│
+;│ output:							│
+;│			rax = length of	source	│
+;└──────────────────────────────────┘
+proc fa_strlen stdcall source:DWORD
+	xor ecx,ecx			; set counter to 0
+    mov esi,[source]	; load source location
+	@@:
+		inc ecx
+		lodsb
+		cmp al,al
+		jnz @B
+	mov eax,ecx
+	ret
+endp
